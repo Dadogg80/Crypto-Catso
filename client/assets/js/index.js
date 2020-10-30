74ee0caccc961a36ff5a4181501e4cb7c69c8770
dadogg80 <ivo.garofalo@gmail.com>
2020-10-30 18:07:30 +0100

    Move Index.js to assets folder

---
+++ client/assets/js/index.js
@@ -0,0 +1,47 @@
+var web3 = new Web3(Web3.givenProvider);
+
+var instance;
+var user;
+var contractAddress = "0x5eF962675bfa8695aEA3477335A2A576A6F276E6";
+
+$(document).ready(function() {
+    window.ethereum.enable().then(function(accounts){
+        instance = new web3.eth.Contract(abi, contractAddress, {from: accounts[0]});
+        user = accounts[0];
+
+        console.log(instance);
+
+        /** Creates an event listener for Birth event emitted by the Smart Contract*/
+        instance.events.Birth().on('data', function(event){
+            console.log(event);
+
+            let owner = event.returnValues.owner;
+            let newCatsoId = event.returnValues.newCatsoId;
+            let mumId = event.returnValues.mumId;
+            let dadId = event.returnValues.dadId;
+            let genes = event.returnValues.genes;
+            $("#catsoInfo").css("display", "block");
+            $("#catsoInfo").text("Congratulations! Your new Catso was created!");
+            $("#catsoCreation").css("display", "block");
+            $("#catsoCreation").text(`Owner : ${owner}. CatsoId : ${newCatsoId}. MumId : ${mumId}. DadId : ${dadId}. Genes : ${genes}.`);
+        })
+        .on('error', console.error);
+    
+    })
+})
+
+
+function createCatso(){
+    let dnaStr = getDna(); 
+
+    instance.methods.createCatsoGen0(dnaStr).send({}, function(error, txHash){
+        if(error){
+            console.warn(error);
+        }
+        else{
+            console.log(txHash);
+            console.log(`New Catso is created to user address ${user} .`);
+            console.log(`Transaction Hash is: \ntxHash: ${txHash} .`);
+        }
+    })
+}
